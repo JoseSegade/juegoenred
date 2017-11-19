a7f24e4c7c75ee21d40eaa33afd294877d77c395
sergiosanchez <sergete86@gmail.com>
2017-11-19 18:40:05 +0100

    Añadido Score y ordenado de mayor a menor falta darle aspecto visual

---
+++ 2 PRACTICA SCORE/gameinvaders/src/main/java/juegoenred/gameinvaders/GameinvadersApplication.java
@@ -0,0 +1,12 @@
+package juegoenred.gameinvaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameinvadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameinvadersApplication.class, args);
+	}
+}
